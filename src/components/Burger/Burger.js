e05396bb39d4edb2f65cfabc40846b9d21eb2b01
alphaborel <aaborel@gmail.com>
2018-07-27 21:20:17 -0700

    building up functionality

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,15 +3,21 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
-  
+
   // our props are an object, thus need some work before we can use them. Object.keys returns an array of keys from the object it's given.
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
   .map(igKey => {
     //this double map first returns an array of ingredients, then maps over that new array, duplicating items based on their quantity in state
     return [...Array(props.ingredients[igKey])].map((_, i)=> {
       return <BurgerIngredient key={igKey +1} type={igKey} />
     })
-  })
+  }).reduce((arr, el) => {
+    return arr.concat(el)
+  }, []);
+  if(transformedIngredients.length === 0){
+      transformedIngredients = <p>Please start adding ingredients!</p>
+  }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top"/>
