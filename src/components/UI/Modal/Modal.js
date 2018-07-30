01b44ad4279f62c161822102b8f032c3e5e47c6a
alphaborel <aaborel@gmail.com>
2018-07-29 20:35:34 -0700

    module 8 complete

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Modal.css';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Backdrop from '../Backdrop/Backdrop'
 
-const modal = (props) => (
-  <Aux>
-  <Backdrop show={props.show} clicked={props.modalClosed}/>
-    <div className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1':'0'
-      }}>
-        {props.children}
-    </div>
-  </Aux>
-)
-export default modal;
+class Modal extends Component {
+
+  //since this modal wraps the OrderSummary component, it doesn't need to always render OrderSummary unless the order button is actually clicked.
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show
+  }
+
+  render() {
+    return (
+      <Aux>
+      <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+        <div className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1':'0'
+          }}>
+            {this.props.children}
+        </div>
+      </Aux>
+    )
+  }
+
+}
+export default Modal;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 
-import Aux from '../../hoc/Aux';
+import Aux from '../../hoc/Aux/Aux';
 import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -99,9 +100,11 @@ class BurgerBuilder extends Component {
         <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
           <OrderSummary
             ingredients={this.state.ingredients}
+            price={this.state.totalPrice}
             purchaseCancelled={this.purchaseCancelHandler}
             purchaseContinued={this.purchaseContinueHandler}/>
         </Modal>
+
         <Burger ingredients={this.state.ingredients}/>
         <BuildControls
           ingredientAdded={this.addIngredientHandler}
